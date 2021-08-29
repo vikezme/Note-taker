8d7cc5a56ebd1e8e6bdc1d4ab6a4d69a6b50b55e
kumar vikrant <vikezme@gmail.com>
2021-08-29 18:10:49 +0930

    adding file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Note-taker
+
+This is a note taking application using Express and Node.js. This application has functionality to add, view, and delete notes that are being stored in a db.json file.
+
+This application requires the following NPM modules:
+
+fs
+Express
+Path
+
+
+Need to install npm packages that aren't already included in node.
+
+The initial page will take you to a welcome/get started page, once you click into get started you can start creating your notes!
--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/notes', (req, res) => {
     res.sendFile('./public/notes.html', { root: __dirname });
 });
 
-//* `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, json) => {
         let obj = JSON.parse(json);
@@ -25,36 +25,28 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
-//* `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file
-// and then return the new note to the client.
-// You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-// {title: "dfg", text: "dfg"}
+
 app.post('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', (err, json) => {
         let obj = JSON.parse(json);
         let newNote = req.body;
         newNote.id = uuidv4();
-        //console.log(newNote);
-        //console.log(obj);
-        // Add the new note
+
         obj.push(newNote);
-        //console.log(obj);
-        // Write back to file
+
         fs.writeFileSync('./db/db.json', JSON.stringify(obj));
         res.json(newNote);
     });
 });
 
-//* `DELETE /api/notes/:id` should receive a query parameter that contains the id of a note to delete.
-// To delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+
 app.delete('/api/notes/:id', (req, res) => {
     fs.readFile('./db/db.json', (err, json) => {
         let obj = JSON.parse(json);
         let deleteId = req.params['id'];
-        // Delete the note
+
         obj = obj.filter(item => item.id != deleteId);
-        //console.log(obj);
-        // Write back to file
+        
         fs.writeFileSync('./db/db.json', JSON.stringify(obj));
         res.status(200).json({status:"ok"});
     });
