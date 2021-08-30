2d659f65f0092dd659348a6b0ead976d9aa34896
kumar vikrant <vikezme@gmail.com>
2021-08-30 18:14:11 +0930

    Adding file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.end.PORT || 3000;
 const fs = require('fs');
 
 const { v4: uuidv4 } = require('uuid');
